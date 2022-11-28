35621bffd9678c9c46b55bc94f0d0fd0a7c27247
mich-hales <michellehales93@gmail.com>
2022-11-28 16:50:52 -0700

    updates to src-sw.js file on client side

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,27 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+
+// Asset caching
+const assetCaching = new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+// deesignate what to cache
+const designateCache = ({ request }) => {
+  return (
+    request.destination === 'style' ||
+    request.destination === 'script' ||
+    request.destination === 'image'
+  );
+};
+
+registerRoute(designateCache, assetCaching);
